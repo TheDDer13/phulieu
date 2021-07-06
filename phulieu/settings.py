a2bc2fe68f9a7b5c5b1fe80c06151a7e51f80e40
TheDDer13 <tranphuongha1234@gmail.com>
2021-07-06 11:53:08 +0700

    Push code

--- phulieu/settings.py
+++ phulieu/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if DEBUG:
 # location where you will store your static files
-    STATICFILES_DIRS = [os.path.join(BASE_DIR,'marquette/static')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 else:
 #location where django collect all static files
     STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
